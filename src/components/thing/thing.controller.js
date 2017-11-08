363a4c7629445d6f451655755b458169d4705f2d
Marcos Cesar <marcos.cesar@ccc.ufcg.edu.br>
2017-11-08 17:08:59 -0200

    Cadastro

--- src/components/thing/thing.controller.js
+++ src/components/thing/thing.controller.js
@@ -7,6 +7,14 @@ angular.module('Invent').controller('ThingController', function($scope, $state) 
 		$state.go(state);
 	};
 
+	myScope.addThing = function(thing){
+		ThingFactory.addThing(thing).then(function(result){
+			myScope.showSuccessMessage('Sucesso ao adicionar!');
+		}).catch(function(result){
+			myScope.showNetworkError('Erro ao adicionar!');
+		})
+	}
+
 	$scope.currentThing = 'Cadeira';
 
 
